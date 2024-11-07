1a2681a7e44e605143260c25cf7a43e886f1cf06
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-07 22:24:02 +0700

    Add feature show hide password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="bg-white max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg px-6 py-8 sm:p-4 rounded-[30px] shadow-md">
       <div className="space-y-2 sm:space-y-4 sm:pt-6 md:pt-8">
@@ -29,14 +33,34 @@ const Login = () => {
           <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
             Masukkan Password
           </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Masukkan password anda"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-            required=""
-          />
+          <div className="flex">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              id="password"
+              placeholder="Masukkan password anda"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-s-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              required=""
+            />
+            <button type="button" onClick={() => setShowPassword(!showPassword)} className="inline-flex items-center px-2 sm:px-4 text-sm text-gray-900 bg-gray-50 hover:bg-gray-200 border border-s-0 border-gray-300 rounded-e-md">
+              {showPassword ? (
+                <svg className="w-6 h-6 text-gray-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="currentColor" viewBox="0 0 24 24">
+                  <path
+                    fillRule="evenodd"
+                    d="M4.998 7.78C6.729 6.345 9.198 5 12 5c2.802 0 5.27 1.345 7.002 2.78a12.713 12.713 0 0 1 2.096 2.183c.253.344.465.682.618.997.14.286.284.658.284 1.04s-.145.754-.284 1.04a6.6 6.6 0 0 1-.618.997 12.712 12.712 0 0 1-2.096 2.183C17.271 17.655 14.802 19 12 19c-2.802 0-5.27-1.345-7.002-2.78a12.712 12.712 0 0 1-2.096-2.183 6.6 6.6 0 0 1-.618-.997C2.144 12.754 2 12.382 2 12s.145-.754.284-1.04c.153-.315.365-.653.618-.997A12.714 12.714 0 0 1 4.998 7.78ZM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg className="w-6 h-6 text-gray-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="currentColor" viewBox="0 0 24 24">
+                  <path d="m4 15.6 3.055-3.056A4.913 4.913 0 0 1 7 12.012a5.006 5.006 0 0 1 5-5c.178.009.356.027.532.054l1.744-1.744A8.973 8.973 0 0 0 12 5.012c-5.388 0-10 5.336-10 7A6.49 6.49 0 0 0 4 15.6Z" />
+                  <path d="m14.7 10.726 4.995-5.007A.998.998 0 0 0 18.99 4a1 1 0 0 0-.71.305l-4.995 5.007a2.98 2.98 0 0 0-.588-.21l-.035-.01a2.981 2.981 0 0 0-3.584 3.583c0 .012.008.022.01.033.05.204.12.402.211.59l-4.995 4.983a1 1 0 1 0 1.414 1.414l4.995-4.983c.189.091.386.162.59.211.011 0 .021.007.033.01a2.982 2.982 0 0 0 3.584-3.584c0-.012-.008-.023-.011-.035a3.05 3.05 0 0 0-.21-.588Z" />
+                  <path d="m19.821 8.605-2.857 2.857a4.952 4.952 0 0 1-5.514 5.514l-1.785 1.785c.767.166 1.55.25 2.335.251 6.453 0 10-5.258 10-7 0-1.166-1.637-2.874-2.179-3.407Z" />
+                </svg>
+              )}
+              <span className="sr-only">{showPassword ? 'Hide' : 'Show'} password</span>
+            </button>
+          </div>
         </div>
         <div className="flex items-start">
           <div className="flex items-start">
@@ -57,7 +81,7 @@ const Login = () => {
             Lost Password?
           </a>
         </div>
-        <button type="submit" className="w-full text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+        <button type="submit" className="w-full text-white bg-red-500 hover:bg-red-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
           Masuk
         </button>
         <div className="text-sm font-medium text-gray-400 text-center">
