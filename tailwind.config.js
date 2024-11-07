37fe8d66aa613c5c47fb221ac5894e8c9138423e
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-07 19:16:17 +0700

    Create custom boxShadow:md

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        md: '0 0px 6px 0px rgba(0, 0, 0 , 0.1)',
+      },
+    },
   },
   plugins: [],
 };
