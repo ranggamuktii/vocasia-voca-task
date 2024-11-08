cad1c1e854cccb5eaf1a544380e76ebe38afccf0
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-08 11:26:05 +0700

    Implement react router for task page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login.jsx';
+import Task from './pages/Task.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/task" element={<Task />} />
       </Routes>
     </Router>
   );
