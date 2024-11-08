4d5564a8603af2222a2a150295d5ad923a98ee63
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-08 12:39:46 +0700

    Implement react router for update profile page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import Task from './pages/Task.jsx';
+import UpdateProfile from './pages/UpdateProfile.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/task" element={<Task />} />
+        <Route path="/updateProfile" element={<UpdateProfile />} />
       </Routes>
     </Router>
   );
