86d732cf18bcc7a101aa4fec543e95bf03792cc5
Rangga Mukti <r4n994mukti@gmail.com>
2024-11-20 13:03:42 +0700

    ADD POST tasks

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -39,6 +39,23 @@ const Task = () => {
     }
   };
 
+  const addTask = async () => {
+    try {
+      const response = await fetch(BASE_URL + '/tasks', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+        body: JSON.stringify({ title: newTask }),
+      });
+      if (!response.ok) throw new Error('Failed to create task');
+
+      const data = await response.json();
+      setTasks([...tasks, data.data]);
+      setNewTask('');
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   useEffect(() => {
     fetchTasks();
   }, []);
@@ -88,7 +105,7 @@ const Task = () => {
             placeholder="Tambahkan tugas baru"
             required=""
           />
-          <button className="bg-red-500 px-2 rounded-xl">
+          <button onClick={addTask} className="bg-red-500 px-2 rounded-xl">
             <svg className="w-6 h-6 text-white hover:scale-110 transition duration-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="currentColor" viewBox="0 0 24 24">
               <path fillRule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z" clipRule="evenodd" />
             </svg>
